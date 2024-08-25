d16238b10ebbe50374d4f7bc73a3ad45c501bf3a
stefanus ayudha <stefanus.ayudha@gmail.com>
2024-08-25 10:14:28 +0700

    fix(Login): fix payload pattern

--- Main/authentication/model/common/authentication/LoginRoute.kt
+++ Main/authentication/model/common/authentication/LoginRoute.kt
@@ -1,12 +1,13 @@
 import core.navigation.NavigationRoute
 
 data class LoginRoute(
-    override val route: String = ROUTE,
     override val payload: LoginRoutePayload = LoginRoutePayload(),
 ) : NavigationRoute<LoginRoutePayload> {
     companion object {
         const val ROUTE = "login"
     }
+
+    override fun route(): String = ROUTE
 }
 
 data class LoginRoutePayload(
